1018b0da21e9435bb44a99ce62315d6fe78c3785
vuden2605 <vuden2605@gmail.com>
2025-04-25 17:31:57 +0700

    up sign out feature

--- Project_ATBM/AdminForm.cs
+++ Project_ATBM/AdminForm.cs
@@ -54,7 +54,20 @@ namespace Project_ATBM
 
         private void button4_Click(object sender, EventArgs e)
         {
+            DialogResult result = MessageBox.Show(
+                "Bạn có chắc chắn muốn đăng xuất không?",
+                "Xác nhận đăng xuất",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question
+            );
 
+            if (result == DialogResult.OK)
+            {
+                this.Hide(); 
+                LoginForm loginForm = new LoginForm();
+                loginForm.FormClosed += (s, args) => this.Close(); 
+                loginForm.Show();
+            }
         }
 
         private void btnDeleteUser_Click(object sender, EventArgs e)
