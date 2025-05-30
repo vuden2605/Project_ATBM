1d72ff19272bc821694b1ce729d32a70e7dcbc07
quocviett0123 <22120432@student.hcmus.edu.vn>
2025-05-30 17:58:59 +0700

    fSINHVIEN

---
+++ Project_ATBM/SinhVienForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_ATBM
+{
+    public partial class SinhVienForm : Form
+    {
+        public SinhVienForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            textBox4.Enabled = true;
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            textBox4.Enabled = true;
+        }
+
+      
+    }
+}
